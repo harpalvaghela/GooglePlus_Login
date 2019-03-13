7ff60298a870467290ab1fe14aed986fc8f9ad84
Harpal Vaghela <harpal.30898@gmail.com>
2019-03-13 23:22:12 +0530

    Initial commit

---
+++ app/src/main/java/com/dev/googleplus_login/activity/HomePageActivity.java
@@ -0,0 +1,16 @@
+package com.dev.googleplus_login.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.dev.googleplus_login.R;
+
+public class HomePageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_page);
+
+
+    }
+}
